83d0255a8c81989c1bcd90bfdea8598b025566b8
choi <choi@192.168.0.70>
2018-12-26 17:56:35 +0900

    test1

--- src/practice01/Prob1.java
+++ src/practice01/Prob1.java
@@ -6,6 +6,7 @@ public class Prob1 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner( System.in );
 		System.out.println("zzzzzzz");
+		System.out.println("zzzzzzz");
 		
 		scanner.close();
 	}
